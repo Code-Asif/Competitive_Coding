b5376b04539d811f069214297ddbf6def773d58f
Code-Asif <safety.asif.alam@gmail.com>
2024-11-19 09:25:08 +0530

    new file:   CompetitiveProgramming/isEven.class 	new file:   CompetitiveProgramming/isEven.java

---
+++ CompetitiveProgramming/isEven.java
@@ -0,0 +1,12 @@
+public class isEven {
+    public static void main(String[] args) {
+        System.out.println("Number is even: "+EvenUsingModulas(12));
+        System.out.println("Number is even: "+EvenUsingBitwise(12));
+    }
+    public static boolean EvenUsingModulas(int n){
+        return n%2==0;
+    }
+    public static boolean EvenUsingBitwise(int n){
+        return (n&1)==0;
+    }
+}
