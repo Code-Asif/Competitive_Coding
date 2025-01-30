1a0f917e71cf2900a01315e0d7016bd2c0a47710
Code-Asif <safety.asif.alam@gmail.com>
2025-01-30 17:55:17 +0530

    new file:   CompetitiveProgramming/NQueenProblem.class 	new file:   CompetitiveProgramming/NQueenProblem.java 	new file:   CompetitiveProgramming/maxConsecutive.java

---
+++ CompetitiveProgramming/maxConsecutive.java
@@ -0,0 +1,13 @@
+public class maxConsecutive {
+    public static void main(String[] args) {
+        int arr[] = {1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1};
+        findMaxOnes(arr);
+    }
+
+    public static void findMaxOnes(int arr[]){
+        int start = 0, end = 0;
+        for(int i=0; i<arr.length; i++){
+            if()
+        }
+    }
+}
